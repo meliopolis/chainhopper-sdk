b1d11fa6fc077e30539d24246f0d47960bc24db8
Greg Phillips <gphil@gphil.net>
2025-08-13 16:38:04 -0400

    WIP: fixing tests

--- src/client.ts
+++ src/client.ts
@@ -201,6 +201,7 @@ export class ChainHopperClient {
       ...rest,
       migrations: [{ destination, path: exactPath }],
     });
+    console.log(sourcePosition);
     if (unavailableMigrations.length > 0) {
       throw new Error(`Specified destination not available:\n  - ${unavailableMigrations[0].reasons.join('\n  - ')}`);
     }
